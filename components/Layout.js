81bdddeb5db85060e4de5c6bfc705874d75fad0e
Dan Stroot <dan@thestroots.com>
2021-10-01 07:54:43 -0700

    added GA

--- components/Layout.js
+++ components/Layout.js
@@ -33,24 +33,6 @@ export default function Layout({ children, meta: pageMeta }) {
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.cardImage} />
-
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `
-          }}
-        />
       </Head>
       <Navbar />
       <main id="skip">{children}</main>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import * as ga from '@/utils/ga';
-
+import Script from 'next/script';
 import '@/styles/main.css';
 import '@/styles/chrome-bug.css';
 
@@ -9,29 +6,20 @@ import Layout from '@/components/Layout';
 import { UserContextProvider } from '@/utils/useUser';
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  useEffect(() => {
-    document.body.classList?.remove('loading');
-  }, []);
-
   return (
     <div className="bg-primary">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="ga-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+      </Script>
       <UserContextProvider>
         <Layout>
           <Component {...pageProps} />
